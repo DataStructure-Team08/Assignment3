995e84d24de250b2c02e3902272c1ff38e17ae67
abos-ksh <kimsh67723@naver.com>
2024-11-09 16:17:07 +0900

    ArrayPol

--- common.c
+++ common.c
@@ -1,8 +1,9 @@
 #include "common.h"
 
 // create new node
-Node* createNode(int coeff, int exp) {
-    Node* newNode = (Node*)malloc(sizeof(Node));
+Node *createNode(int coeff, int exp)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node));
     newNode->coefficient = coeff;
     newNode->exponent = exp;
     newNode->next = NULL;
@@ -10,22 +11,27 @@ Node* createNode(int coeff, int exp) {
 }
 
 // input linkedlist-version polynomial
-Node* inputLinkedPolynomial() {
-    Node* head = NULL;
-    Node* tail = NULL;
+Node *inputLinkedPolynomial()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
     int coeff, exp;
 
     printf("Enter polynomial terms (input coefficient and exponent, end with -1 -1):\n");
-    while (1) {
+    while (1)
+    {
         scanf("%d %d", &coeff, &exp);
-        if (coeff == -1 && exp == -1) break;
+        if (coeff == -1 && exp == -1)
+            break;
 
-        Node* newNode = createNode(coeff, exp);
-        if (!head) {
+        Node *newNode = createNode(coeff, exp);
+        if (!head)
+        {
             head = newNode;
             tail = newNode;
         }
-        else {
+        else
+        {
             tail->next = newNode;
             tail = newNode;
         }
@@ -34,28 +40,39 @@ Node* inputLinkedPolynomial() {
 }
 
 // input array-version polynomial
-int* inputArrayPolynomial() {
-    int* arrayPol=NULL;
+Pol *inputArrayPolynomial()
+{
+    Pol *result;
+    int *arrayPol = NULL;
     int cnt = 0;
     int coeff, exp;
 
     printf("Enter polynomial terms (input coefficient and exponent, end with -1 -1):\n");
-    while (1) {
+    while (1)
+    {
         scanf("%d %d", &coeff, &exp);
-        if (coeff == -1 && exp == -1) break;
-        if (cnt == 0) arrayPol = (int*)calloc(exp+1,sizeof(int));
+        if (coeff == -1 && exp == -1)
+            break;
+        if (cnt == 0)
+        {
+            arrayPol = (int *)calloc(exp + 1, sizeof(int));
+            result->size = exp;
+        }
         arrayPol[exp] = coeff;
         cnt++;
     }
-
-    return arrayPol;
+    result->a = arrayPol;
+    return result;
 }
 
 // print linkedlist-version polynomial
-void printLinkedPolynomial(Node* poly) {
-    Node* current = poly;
-    while (current) {
-        if (current->coefficient > 0 && current != poly) {
+void printLinkedPolynomial(Node *poly)
+{
+    Node *current = poly;
+    while (current)
+    {
+        if (current->coefficient > 0 && current != poly)
+        {
             printf("+");
         }
         printf("%dx^%d ", current->coefficient, current->exponent);
@@ -65,13 +82,31 @@ void printLinkedPolynomial(Node* poly) {
 }
 
 // oprint array-version polynomial
-void printArrayPolynomial(int* a, int n) {
-    for (int i = 0; i < n; i++)
-        printf("%d ", a[i]);
+void printArrayPolynomial(int *a)
+{
+    int i = 0;
+    while (!a)
+    {
+        if (a[i] != 0)
+            printf("%dx^%d + ", a[i], i);
+        i++;
+    }
     printf("\n");
 }
 
-// 
-void freeArray(int* arr) {
+// empty polynomial
+Node *freeNode(Node *head)
+{
+    Node *temp;
+    while (head != NULL)
+    {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+//
+void freeArray(int *arr)
+{
     free(arr);
 }
--- common.h
+++ common.h
@@ -7,17 +7,25 @@
 #include <stdlib.h>
 
 // 다항식의 항을 나타내는 노드 구조체
-typedef struct Node {
-    int coefficient; // 계수
-    int exponent;    // 차수
-    struct Node* next; // 다음 노드의 포인터
+typedef struct Node
+{
+    int coefficient;   // 계수
+    int exponent;      // 차수
+    struct Node *next; // 다음 노드의 포인터
 } Node;
 
-Node* createNode(int coeff, int exp);
-Node* inputLinkedPolynomial();
-int* inputArrayPolynomial();
-void printPolynomial(Node* poly);
-void print_array(int* a, int n);
-void freeArray(int* arr);
+typedef struct Pol
+{
+    int size;
+    int *a;
+} Pol;
 
-#endif 
+Node *createNode(int coeff, int exp);
+Node *inputLinkedPolynomial();
+Pol *inputArrayPolynomial();
+void printPolynomial(Node *poly);
+void print_array(int *a, int n);
+Node *freeNode(Node *head);
+void freeArray(int *arr);
+
+#endif
