820311df155917fabf9f2f5949eb30c6e4f6d0e0
Lumi327 <70458871+Lumi327@users.noreply.github.com>
2024-11-09 14:48:37 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,19 +4,35 @@
 #include "cal.h"
 
 int main() {
-    printf("First polynomial:\n");
-    Node* poly1 = inputLinkedPolynomial();
+    printf("First array-version polynomial: ");
+    int* arr1 = inputArrayPolynomial();
 
-    printf("Second polynomial:\n");
-    Node* poly2 = inputLinkedPolynomial();
+    printf("Second array-version polynomial: ");
+    int* arr2 = inputArrayPolynomial();
 
-    Node* result = addLinkedPolynomials(poly1, poly2);
+    int* link_add = addArrayPolynomials(arr1, arr2);
+    int* link_sub = subArrayPolynomials(arr1, arr2);
 
-    printf("Result polynomial:\n");
+    printf("Result:");
+    printArrayPolynomial(result);
+    printf("\n");
+    
+    printf("First linked-list version polynomial: ");
+    Node* link1 = inputLinkedPolynomial();
+
+    printf("Second linked-list version polynomial: ");
+    Node* link2 = inputLinkedPolynomial();
+
+    Node* link_add = addLinkedPolynomials(link1, link2);
+    Node* link_sub = subLinkedPolynomials(link1, link2);
+
+    printf("Result:");
     printLinkedPolynomial(result);
+    printf("\n");
 
     // 메모리 해제
     // TODO: 각 링크드 리스트의 노드를 해제하는 코드를 추가하세요.
+    
 
     return 0;
 }
