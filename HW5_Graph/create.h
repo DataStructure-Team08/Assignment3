3862fd3fae655f29ab49c22272f552482fa7818f
Lumi327 <70458871+Lumi327@users.noreply.github.com>
2024-11-30 00:27:47 +0900

    Update create.h

--- HW5_Graph/create.h
+++ HW5_Graph/create.h
@@ -2,11 +2,11 @@
 #define CREATE
 
 int** createAdjacencyMatrix(int numNodes);
-int** copyAdjacencyMatrix(int** graph, int numNodes);
 void addRandomEdges(int** graph, int numNodes, int numEdges);
 void printAdjacencyMatrix(int** graph, int numNodes);
 void freeAdjacencyMatrix(int** graph, int numNodes);
-void separateGraph(int** graph, int numNodes);
+
+void copyGraph(int** source, int** destination, int numNodes);
 
 #endif
 
