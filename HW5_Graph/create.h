af45843b34c06f6b338738f2a7b0874ec4b419e5
Lumi327 <70458871+Lumi327@users.noreply.github.com>
2024-11-29 22:45:47 +0900

    Update create.h

--- HW5_Graph/create.h
+++ HW5_Graph/create.h
@@ -1,11 +1,12 @@
 #ifndef CREATE
 #define CREATE
 
-// 그래프 생성 및 관리
-int** createAdjacencyMatrix(int numNodes);        // 동적 그래프 생성
-void freeAdjacencyMatrix(int** graph, int numNodes); // 그래프 메모리 해제
-void addRandomEdges(int** graph, int numNodes, int numEdges); // 랜덤 간선 추가
-void printAdjacencyMatrix(int** graph, int numNodes); // 그래프 출력
+int** createAdjacencyMatrix(int numNodes);
+int** copyAdjacencyMatrix(int** graph, int numNodes);
+void addRandomEdges(int** graph, int numNodes, int numEdges);
+void printAdjacencyMatrix(int** graph, int numNodes);
+void freeAdjacencyMatrix(int** graph, int numNodes);
+void separateGraph(int** graph, int numNodes);
 
-#endif // CREATE_H
+#endif
 
