eb39d1661315ea597ac502faacdaad54e4898a02
minji57 <minwl0507@naver.com>
2024-11-30 12:19:34 +0900

    Update minspantree.h

--- HW5_Graph/minspantree.h
+++ HW5_Graph/minspantree.h
@@ -2,8 +2,8 @@
 #define MINSPANTREE
 
 // Function declarations for Prim's and Kruskal's algorithms
-void primMST(int** graph, int numNodes);
-void kruskalMST(int** graph, int numNodes);
+int** primMST(int** graph, int numNodes);
+int** kruskalMST(int** graph, int numNodes);
 
 // Function to check if a graph is a Spanning Tree
 bool isSpanningTree(int** graph, int numNodes);
