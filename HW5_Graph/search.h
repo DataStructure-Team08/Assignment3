51a227d346d012701947e5dac7c82fe3e2811dfb
Lumi327 <70458871+Lumi327@users.noreply.github.com>
2024-11-30 11:25:08 +0900

    Update search.h

--- HW5_Graph/search.h
+++ HW5_Graph/search.h
@@ -5,7 +5,7 @@
 
 // BFS, DFS, Connected Component
 void bfs(int** graph, int numNodes, int start); // BFS 구현
-void dfs(int** graph, int numNodes, int start, bool* visited); // DFS 구현
+void dfs(int** graph, int numNodes, int start, bool* visited, bool print); // DFS 구현
 int* findConnectedComponents(int** graph, int numNodes); // 연결 성분 탐색
 
 // DFS 기반 사이클 판별
@@ -13,4 +13,5 @@ bool isCyclicDFS(int** graph, int numNodes);
 bool isCyclicDFSUtil(int** graph, bool* visited, int current, int parent, int numNodes);
 
 #endif // SEARCH_H
+
 
